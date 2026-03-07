fcbccd13d9668afd5b6412097ee882fa82e2e17d
Garciiaajuank <juankqueve@gmail.com>
2026-03-06 22:30:51 -0500

    subir archivo

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+
+@app.route('/')
+def home(): 
+    return render_template ('Garcia.html')
+
+@app.route('/FirstPage/<name>')
+def firstPage(name): 
+    return render_template ('Garcia.html',name=name)

b8956ead96a74b68d55d380916dafdc96a6bff05
Andres5-12 <corredor011andres@gmail.com>
2026-03-06 22:47:16 -0500

    Comienzo de trabajo

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask (__name__)
 def home(): 
     return render_template ('Garcia.html')
 
-@app.route('/Andres')
-def home2(): 
-    return render_template ('Corredor.html')
 
 @app.route('/FirstPage/<name>')
 def firstPage(name): 

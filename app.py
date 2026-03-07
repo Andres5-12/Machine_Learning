348f0d5794e32e5d67760994bb4a994bf79d15f6
Andres5-12 <corredor011andres@gmail.com>
2026-03-06 23:00:07 -0500

    Primer carga ramaAndres

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask (__name__)
 def home(): 
     return render_template ('Garcia.html')
 
+@app.route('/Andres')
+def homeAndres(): 
+    return render_template ('Corredor.html')
 
 @app.route('/FirstPage/<name>')
 def firstPage(name): 
